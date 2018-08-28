507eda84452a039e13e727b45b1d15ffe72b2fae
Daniel <d_olaya@hotmail.com>
2018-08-28 10:54:13 -0700

    killdaemon.sh added liquidd

--- src/killdaemon.sh
+++ src/killdaemon.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-
-num=( $(ps aux | grep bitcoind | awk '$11 ~ /^bitcoin/' | awk '{print$2}' | awk '{print NR}' | wc -l));
-
+# Killing bitcoind processes
 let -a arr;
+num=( $(ps aux | grep bitcoind | awk '$11 ~ /^bitcoin/' | awk '{print$2}' | awk '{print NR}' | wc -l));
 arr=( $(ps aux | grep bitcoind | awk '$11 ~ /^bitcoin/' | awk '{print$2}'));
 
-echo "[Info - killdaemon.sh] Number of daemons to kill [${num}]";
+echo "[Info - killdaemon.sh] Number of bitcoind processes to terminate [${num}]";
 counter=0;
 while [ ${counter} -lt ${num} ]
 do
 	kill -9 ${arr[$counter]}
-	echo "[Info - killdaemon.sh] Killing process ${arr[$counter]} ..."
+	echo "[Info - killdaemon.sh] Terminating process ${arr[$counter]} ..."
 	((counter++))
 done
+echo "[Info - killdaemon.sh] All bitcoind processes terminated"
 
-echo "[Info - killdaemon.sh] All processes terminated"
+
+# Killing liquidd processes
+let -a arr;
+num=( $(ps aux | grep liquidd | awk '$11 ~ /^liquidd/' | awk '{print$2}' | awk '{print NR}' | wc -l));
+arr=( $(ps aux | grep liquidd | awk '$11 ~ /^liquidd/' | awk '{print$2}'));
+
+echo "[Info - killdaemon.sh] Number of liquidd processes to terminate [${num}]";
+counter=0;
+while [ ${counter} -lt ${num} ]
+do
+	kill -9 ${arr[$counter]}
+	echo "[Info - killdaemon.sh] Terminating process ${arr[$counter]} ..."
+	((counter++))
+done
+echo "[Info - killdaemon.sh] All liquidd processes terminated"
